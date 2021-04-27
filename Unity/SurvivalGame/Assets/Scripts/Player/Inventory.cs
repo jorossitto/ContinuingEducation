f63565623cf6035fd7260be68616a5e115a2b965
jorossitto <jorossitto@hotmail.com>
2021-04-27 17:53:58 -0400

    Survival Game

--- Unity/SurvivalGame/Assets/Scripts/Player/Inventory.cs
+++ Unity/SurvivalGame/Assets/Scripts/Player/Inventory.cs
@@ -29,6 +29,9 @@ public class Inventory : MonoBehaviour
     public GameObject dropButton;
 
     private int curEquipIndex;
+    private bool isEquipable;
+    private bool isEquipped;
+    private bool isConsumable;
 
     //components
     private PlayerController controller;
@@ -58,11 +61,31 @@ public class Inventory : MonoBehaviour
             UISlots[x].index = x;
             UISlots[x].Clear();
         }
+
+        ClearSelectedItemWindow();
     }
 
+    public void OnInventoryButton(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started)
+        {
+            Toggle();
+        }
+
+    }
     public void Toggle()
     {
-
+        if(inventoryWindow.activeInHierarchy)
+        {
+            inventoryWindow.SetActive(false);
+            onCloseInventory.Invoke();
+        }
+        else
+        {
+            inventoryWindow.SetActive(true);
+            onOpenInventory.Invoke();
+            ClearSelectedItemWindow();
+        }
     }
 
     public bool IsOpen()
@@ -96,37 +119,92 @@ public class Inventory : MonoBehaviour
         ThrowItem(item);
     }
 
+    //spawns item in front of the player
     void ThrowItem(ItemData item)
     {
-
+        Instantiate(item.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360.0f));
     }
 
     void UpdateUI()
     {
         for(int x = 0; x < slots.Length; x++)
         {
-
+            if(slots[x].item != null)
+            {
+                UISlots[x].Set(slots[x]);
+            }
+            else
+            {
+                UISlots[x].Clear();
+            }
         }
     }
 
     ItemSlot GetItemStack(ItemData item)
     {
+        for(int x = 0; x < slots.Length; x++)
+        {
+            if(slots[x].item == item && slots[x].quantity < item.maxStackAmount)
+            {
+                return slots[x];
+            }
+        }
         return null;
     }
 
+    //returns an empty slot in the inventory
+    //if there are no empty slots - return null
     ItemSlot GetEmptySlot()
     {
+        for(int x = 0; x < slots.Length; x++)
+        {
+            if(slots[x].item == null)
+            {
+                return slots[x];
+            }
+        }
         return null;
     }
 
     public void SelectItem (int index)
     {
+        if(slots[index].item == null)
+        {
+            return;
+        }
 
+        selectedItem = slots[index];
+        selectedItemIndex = index;
+        selectedItemName.text = selectedItem.item.displayName;
+        selectedDescription.text = selectedItem.item.description;
+
+        //todo set stat values and stat names
+
+
+        isConsumable = selectedItem.item.type == ItemType.Consumable;
+        isEquipable = selectedItem.item.type == ItemType.Equipable;
+        isEquipped = UISlots[index].equipped;
+
+        useButton.SetActive(isConsumable);
+        equipButton.SetActive(isEquipable && !isEquipped);
+        unEquipButton.SetActive(isEquipable && isEquipped);
+        dropButton.SetActive(true);
     }
 
     void ClearSelectedItemWindow()
     {
+        //clear text elements
+        selectedItem = null;
+        selectedItemName.text = string.Empty;
+        selectedDescription.text = string.Empty;
+        selectedItemStatNames.text = string.Empty;
+        selectedItemStatValues.text = string.Empty;
 
+        //disable buttons
+        useButton.SetActive(false);
+        equipButton.SetActive(false);
+        unEquipButton.SetActive(false);
+        dropButton.SetActive(false);
     }
     public void OnUsebutton()
     {
